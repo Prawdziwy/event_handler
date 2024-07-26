b9ab4ad63e00a89faa03bfe4859ff38fc45348c4
Prawdziwy <damianes7@wp.pl>
2024-07-26 23:54:47 +0200

    Fix formating of app layout, create in calendars

--- resources/views/pages/calendars/create.blade.php
+++ resources/views/pages/calendars/create.blade.php
@@ -2,45 +2,45 @@
 
 @section('content')
 <section id="page">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Create Calendar') }}</div>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">{{ __('Create Calendar') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('calendars.store') }}">
-                            @csrf
+					<div class="card-body">
+						<form method="POST" action="{{ route('calendars.store') }}">
+							@csrf
 
-                            <!-- Calendar Name Field -->
-                            <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Calendar Name') }}</label>
+							<!-- Calendar Name Field -->
+							<div class="row mb-3">
+								<label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Calendar Name') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
-                                        value="{{ old('name') }}" required>
+								<div class="col-md-6">
+									<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+										value="{{ old('name') }}" required>
 
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
+									@error('name')
+										<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+										</span>
+									@enderror
+								</div>
+							</div>
 
-                            <!-- Submit Button -->
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Create Calendar') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+							<!-- Submit Button -->
+							<div class="row mb-0">
+								<div class="col-md-8 offset-md-4">
+									<button type="submit" class="btn btn-primary">
+										{{ __('Create Calendar') }}
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 @endsection
