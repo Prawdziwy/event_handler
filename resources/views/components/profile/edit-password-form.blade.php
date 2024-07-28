9f984e1d4620652b83407cde5e1b1c8ee4d03fef
Prawdziwy <damianes7@wp.pl>
2024-07-29 00:05:13 +0200

    Fix edit-password-form subpage by adding missing content

--- resources/views/components/profile/edit-password-form.blade.php
+++ resources/views/components/profile/edit-password-form.blade.php
@@ -5,7 +5,8 @@
         <label for="current_password" class="col-md-4 col-form-label text-md-end">{{ __('Current Password') }}</label>
 
         <div class="col-md-6">
-            <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" required>
+            <input id="current_password" type="password"
+                class="form-control @error('current_password') is-invalid @enderror" name="current_password" required>
 
             @error('current_password')
                 <span class="invalid-feedback" role="alert">
@@ -19,7 +20,8 @@
         <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('New Password') }}</label>
 
         <div class="col-md-6">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                name="password" required>
 
             @error('password')
                 <span class="invalid-feedback" role="alert">
@@ -30,10 +32,27 @@
     </div>
 
     <div class="row mb-3">
-        <label for="password_confirmation" class="col-md-4 col-form-label text-md-end">{{ __('Confirm New Password') }}</label>
+        <label for="password_confirmation"
+            class="col-md-4 col-form-label text-md-end">{{ __('Confirm New Password') }}</label>
 
         <div class="col-md-6">
-            <input id="password_confirmation" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" required>
+            <input id="password_confirmation" type="password"
+                class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation"
+                required>
 
             @error('password_confirmation')
-                <span class="invalid-feedback" role=
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+
+    <div class="row mb-0">
+        <div class="col-md-8 offset-md-4">
+            <button type="submit" class="btn btn-primary">
+                {{ __('Update Password') }}
+            </button>
+        </div>
+    </div>
+</form>
