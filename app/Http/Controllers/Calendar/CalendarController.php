89aca627ee95f5e26966e4860f00019ca75e59ee
Prawdziwy <damianes7@wp.pl>
2024-07-28 11:27:31 +0200

    Move CalendarController to Calendar folder for better organization

--- app/Http/Controllers/Calendar/CalendarController.php
+++ app/Http/Controllers/Calendar/CalendarController.php
@@ -1,7 +1,7 @@
 <?php
+namespace App\Http\Controllers\Calendar;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\Models\Calendar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
