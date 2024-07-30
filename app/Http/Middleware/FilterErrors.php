2b20d1be8514f45380dd2f27809e55edd36eebe0
Prawdziwy <damianes7@wp.pl>
2024-07-31 00:47:17 +0200

    Remove unused variable view in error filtering

--- app/Http/Middleware/FilterErrors.php
+++ app/Http/Middleware/FilterErrors.php
@@ -6,18 +6,9 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\View;
-use Illuminate\Contracts\View\Factory as ViewFactory;
 
 class FilterErrors
 {
-    protected ViewFactory $view;
-
-    public function __construct(ViewFactory $view)
-    {
-        $this->view = $view;
-    }
-
     /**
      * Handle an incoming request.
      *
