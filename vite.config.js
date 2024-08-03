c1f7d42716f3193c533ca89b22fa1b33afea8146
Prawdziwy <damianes7@wp.pl>
2024-08-04 00:48:21 +0200

    Update Vite configuration to include calendar and alerts

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/calendar.js',
+                'resources/js/alerts.js',
             ],
             refresh: true,
         }),
